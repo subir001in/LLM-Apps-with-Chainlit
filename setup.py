04095709d512bdb0feac12b0d430905cf993874a
subir001in <40736646+subir001in@users.noreply.github.com>
2024-01-23 15:34:52 -0600

    Chainlit Interface Template checked in on 1/23

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name= 'Generative AI Project',
+    version= '0.0.0',
+    author= 'Subir',
+    author_email= 'samantaray.subir@gmail.com',
+    packages= find_packages(),
+    install_requires = []
+)
---
+++ src/llm.py
@@ -0,0 +1,16 @@
+from openai import OpenAI
+from src.prompt import system_instruction
+
+client = OpenAI()
+
+messages = [
+    {"role":"system","content":system_instruction}
+]
+
+def generate_document(messages, model="gpt-3.5-turbo", tempurature= 0):
+    response = client.chat.completions.create(
+    model=model,
+    messages= messages,
+    tempurature=tempurature
+    )
+    return response['choices'][0]['message']['content']
