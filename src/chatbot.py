a7735fe249d015a9b5b0f4a055d68793f436e100
subir001in <40736646+subir001in@users.noreply.github.com>
2024-02-02 01:11:14 -0600

    updated the Document Loader Component

--- src/chatbot.py
+++ src/chatbot.py
@@ -2,9 +2,11 @@
 
 import os
 import sys
+import pandas as pd
+
 from dotenv import load_dotenv,find_dotenv
 
-from src.helper import docLoader
+from src.helper import doc_Loader,doc_Split
 
 from openai import OpenAI
 OpenAI.api_key = os.environ.get('OPENAI_API_KEY')
@@ -13,9 +15,23 @@ print(f"OPENAI_API_KEY : {OpenAI.api_key}")
 path = sys.path.append('../..')
 
 docPathList =  [os.path.abspath(f'src\Documents\{a}') for a in os.listdir("src\Documents")]
-print(f"docPathList: {docPathList}")
+#print(f"docPathList: {docPathList}")
 
-# Load the Document
-docs = docLoader(docPathList)
-print(len(docs))
+# 1- Load the Document
+pages = doc_Loader(docPathList)
+print(f"Pages = {pages}")
+#print(len(pages))
 
+# 2- Split the Document
+df = pd.DataFrame()
+
+docSplits = doc_Split(pages)
+
+for split in docSplits:
+    #df.add(split.page_content,split.metadata)
+    print("\n@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@\n")
+    print(split.page_content)
+    #print(split.metadata)
+
+# 3- Vectorstores and Embeddings
+    
--- src/helper.py
+++ src/helper.py
@@ -1,7 +1,8 @@
 from langchain_community.document_loaders import PyPDFLoader
+from langchain.text_splitter import RecursiveCharacterTextSplitter
 
-# PDF LOADER
-def docLoader(docList):
+# 1 - PDF LOADER
+def doc_Loader(docList):
     """
     Load PDF using pypdf into array of documents, where each document contains the page content and metadata with page number.
     
@@ -17,10 +18,48 @@ def docLoader(docList):
     #docList = ['D:\\SUBIR\\LEARNING\\GenAI\\ChatBot\\chainlit-chatbot-interface\\LLM-Apps-with-Chainlit\\MSA.docx', 'D:\\SUBIR\\LEARNING\\GenAI\\ChatBot\\chainlit-chatbot-interface\\LLM-Apps-with-Chainlit\\MSA.pdf']
 
     loaders = [PyPDFLoader(doc) for doc in docList]
-    docs = []
+    pages = []
     
     for loader in loaders:
-        docs.append(loader.load())
+        pages.append(loader.load())
 
-    print(f"docs: {docs}")
-    return docs
+    #print(f"docs: {pages[0]}")
+    #print(len(pages))
+
+    return pages[0]
+
+
+# 2 - SPLIT DOCUMENT
+# Reference: https://python.langchain.com/docs/modules/data_connection/document_transformers/
+
+def doc_Split(pages):
+    """
+    Split the documents into smaller chunks.
+    
+    ------------------------------
+    Parameters:
+        docList : Document
+    ------------------------------
+
+    Returns back pages as list of Document Objects.
+    Document(page_content='', metadata={'source': 'D:\\SUBIR\\LEARNING\\GenAI\\ChatBot\\chainlit-chatbot-interface\\LLM-Apps-with-Chainlit\\src\\Documents\\MSA.pdf', 'page': 0})
+
+    """
+
+    r_splitter= RecursiveCharacterTextSplitter(
+        chunk_size= 50,
+        chunk_overlap= 5,
+        
+        separators=[". "]
+        #separators= [". ","[1-12]","[.|((?<=\\n)|(?<=\\r))]"]
+        #separators= ["\n\n","\n","."," ",""]
+    )
+
+    docSplits= r_splitter.split_documents(pages)
+
+
+    print(f"DocSplits =  {docSplits}")
+    print(f"Length of Pages: {len(pages)}")
+    print(f"Length of DocSplits: {len(docSplits)}")
+
+    return docSplits
